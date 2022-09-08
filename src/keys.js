271f97eaa750b4f03caf96c4587a7d8cba7344ab
lriverarivero33 <lriverarivero33@correo.unicordoba.edu.co>
2022-09-08 08:52:33 -0500

    Summary

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongodb: {
+    URI: 'mongodb+srv://admin:<password>@cluster0.kxsse.mongodb.net/?retryWrites=true&w=majority  '
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const engine = require('ejs-mate');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const morgan = require('morgan');
+
+// initializations
+const app = express();
+require('./database');
+require('./passport/local-auth');
+
+// settings
+/*  */
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+// middlewares
+/* The above code is setting up the middleware for the application. */
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+  secret: 'mysecretsession',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Setting the flash messages and the user to the app.locals object. */
+app.use((req, res, next) => {
+  app.locals.signinMessage = req.flash('signinMessage');
+  app.locals.signupMessage = req.flash('signupMessage');
+  app.locals.user = req.user;
+  console.log(app.locals)
+  next();
+});
+
+// routes
+app.use('/', require('./routes/index'));
+
+// Starting the server
+/* Starting the server on the port that is set in the settings. */
+app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
+});
